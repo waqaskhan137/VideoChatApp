1b99905578e6f32f543f961e91efe4064c44c98e
rana.waqas <rana.waqas@afiniti.com>
2022-10-29 16:07:25 +0500

    First commit

---
+++ src/main/java/com/luminent/videochatapp/VideoChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.luminent.videochatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoChatAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luminent/videochatapp/VideoChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luminent.videochatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoChatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
